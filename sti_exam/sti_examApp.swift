1b58dee468b759555e7c30815559788c74a79c1c
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-01-08 14:51:07 +0100

    Initial Commit

---
+++ sti_exam/sti_examApp.swift
@@ -0,0 +1,17 @@
+//
+//  sti_examApp.swift
+//  sti_exam
+//
+//  Created by Andreas Antonsson on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct sti_examApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
