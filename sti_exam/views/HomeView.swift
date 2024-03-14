1e061a2f5de45e95dcad258acddbb14df16ab93e
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-03-14 11:37:13 +0100

    Feat - Firebase setup

---
+++ sti_exam/views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  sti_exam
+//
+//  Created by Andreas Antonsson on 2024-03-14.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
